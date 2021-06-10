55184b306e8bb6e765aff175807d4bea4f3486e4
Mateusz Klimek <matkk11@gmail.com>
2021-06-10 11:08:38 +0200

    Make tests run twice on 2 different days

--- integration_tests/run_tests.py
+++ integration_tests/run_tests.py
@@ -1,15 +1,18 @@
 import os
 import copy
 import yaml
+from datetime import datetime, timedelta
 
 CHECK_DBS = ['postgres', 'snowflake', 'redshift', 'bigquery']
+
+RUN_TIME = datetime(2021, 5, 2, 0, 0, 0)
 
 DBT_VARS = {
     're_data:alerting_z_score': 3,
     're_data:schemas': ['re_data_raw'],
-    're_data:time_window_start': '2021-05-01 00:00:00',
-    're_data:time_window_end': '2021-05-02 00:00:00',
-    're_data:anomaly_detection_window_start': '2021-04-01 00:00:00',
+    're_data:time_window_start': (RUN_TIME - timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S"),
+    're_data:time_window_end': RUN_TIME.strftime("%Y-%m-%d %H:%M:%S"),
+    're_data:anomaly_detection_window_start': (RUN_TIME - timedelta(days=30)).strftime("%Y-%m-%d %H:%M:%S"),
     're_data:activey_monitored_by_default': True
 }
 
@@ -39,8 +42,19 @@ def test_dbt(debug=True):
         if debug:
             run_re_data = 'DBT_MACRO_DEBUGGING=1 ' + run_re_data
 
+        print (f"Running for first day of data")
         os.system(run_re_data)
-        print (f"Computing re_data metrics completed for {db}")
+
+        # updat dbts_vars to run dbt for next day of data
+        dbt_vars['re_data:time_window_start'] = dbt_vars['re_data:time_window_end']
+        dbt_vars['re_data:time_window_end'] = (RUN_TIME + timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S")
+
+        re_data_next_day = 'dbt run {} --vars "{}"'.format(profile_part, yaml.dump(dbt_vars))
+        if debug:
+            re_data_next_day = 'DBT_MACRO_DEBUGGING=1 ' + re_data_next_day
+        
+        print (f"Running for second day of data")
+        os.system(re_data_next_day)
 
         print (f"Running tests for {db}")
         test_re_data = 'dbt test {} --vars "{}"'.format(profile_part, yaml.dump(dbt_vars))
