1c708b910ba28f1028843ee82a58796b94c3982d
Mateusz Klimek <matkk11@gmail.com>
2021-08-18 17:39:06 +0200

    Add support for multiple dbs monitored (only for bigquery / snowflake)

--- macros/meta/information_schema.sql
+++ macros/meta/information_schema.sql
@@ -1,33 +1,39 @@
 {% macro get_monitored_columns(for_schema) %}
-    {{ adapter.dispatch('get_monitored_columns')(for_schema) }}
+    {% set splitted = for_schema.split('.') %}
+    {% set splitted_length = splitted | length %}
+    {% if splitted_length == 2 %}
+        {{ adapter.dispatch('get_monitored_columns')(splitted[1], splitted[0]) }}
+    {% else %}
+        {{ adapter.dispatch('get_monitored_columns')(splitted[0], None) }}
+    {% endif %}
 {% endmacro %}
 
-{% macro default__get_monitored_columns(for_schema) %}
+{% macro default__get_monitored_columns(table_schema, db_name) %}
     select
-        {{table_name() }} as table_name,
+        {{full_table_name(table_name, table_schema, table_catalog) }} as table_name,
         column_name,
         data_type,
         is_nullable,
         {{- is_datetime('data_type') -}} as is_datetime,
         {{- time_filter('column_name', 'data_type') -}} as time_filter
     from
-    {{ tables_in_schema(for_schema) }}
+    {{ tables_in_schema(table_schema, db_name) }}
 {% endmacro %}
 
-{% macro redshift__get_monitored_columns(for_schema) %}
+{% macro redshift__get_monitored_columns(table_schema, db_name) %}
 
     {%- call statement('columns', fetch_result=True) -%}
     select
-        {{table_name() }} as table_name,
+        {{full_table_name(table_name, table_schema, table_catalog) }} as table_name,
         column_name,
         data_type,
         is_nullable,
         {{- is_datetime('data_type')}} as is_datetime,
         {{- time_filter('column_name', 'data_type') -}} as time_filter
     from 
-        information_schema.columns
+        {% if db_name %}{{db_name}}.{% endif %}information_schema.columns
     where
-        table_schema = '{{ for_schema }}'
+        table_schema = '{{ table_schema }}'
     {% endcall %}
 
     {%- set columns = load_result('columns')['data'] -%}
@@ -45,16 +51,16 @@
 
 {% endmacro %}
 
-{% macro tables_in_schema(for_schema) %}
-    {{ adapter.dispatch('tables_in_schema')(for_schema) }}
+{% macro tables_in_schema(schema_name, db_name) %}
+    {{ adapter.dispatch('tables_in_schema')(schema_name, db_name) }}
 {% endmacro %}
 
-{% macro default__tables_in_schema(for_schema) %}
-    INFORMATION_SCHEMA.columns
+{% macro default__tables_in_schema(schema_name, db_name) %}
+    {% if db_name %} {{ db_name}} . {% endif %} INFORMATION_SCHEMA.columns
     where
-    table_schema = '{{ for_schema }}'
+    table_schema = '{{ schema_name }}'
 {% endmacro %}
 
-{% macro bigquery__tables_in_schema(for_schema) %}
-    `{{for_schema}}` . `INFORMATION_SCHEMA`.`COLUMNS`
+{% macro bigquery__tables_in_schema(schema_name, db_name) %}
+    {% if db_name %} `{{ db_name}}` . {% endif %} `{{schema_name}}` . `INFORMATION_SCHEMA`.`COLUMNS`
 {% endmacro %}
