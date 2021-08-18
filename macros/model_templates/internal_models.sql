b409ec26100a2ed4da7ea06040c151da9c2e2e84
Mateusz Klimek <matkk11@gmail.com>
2021-08-18 23:16:25 +0200

    Add feature to not do query execution on compile time

--- macros/model_templates/internal_models.sql
+++ macros/model_templates/internal_models.sql
@@ -13,9 +13,8 @@
 {% endmacro %}
 
 {% macro re_data_last_base_metrics_thread(num) %}
-
     {% set part_name = 're_data_last_base_metrics_part' ~ num %}
-    {{ generate_depends(['re_data_columns', part_name]) }}
+    {{ generate_depends(['re_data_columns', 're_data_run_started_at', 're_data_tables', part_name]) }}
 
     {{
         config(
@@ -23,7 +22,10 @@
         )
     }}
 
-    {{ compute_metrics_for_tables(num, part_name) }}
+    {% if not in_compile() %}
+        {{ compute_metrics_for_tables(num, part_name) }}
+    {% endif %}
+
     {{ empty_last_base_metrics() }}
 
 {% endmacro %}
