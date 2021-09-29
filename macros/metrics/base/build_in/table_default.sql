13128f08340cf08600fb71d7d6020d5bb56c2fa6
Deji Ibrahim <deji.a.ibrahim@gmail.com>
2021-09-29 22:00:38 +0100

    remove config from table default metrics where not used

--- macros/metrics/base/build_in/table_default.sql
+++ macros/metrics/base/build_in/table_default.sql
@@ -1,9 +1,9 @@
 
-{% macro re_data_metric_row_count(time_filter, config) %}
+{% macro re_data_metric_row_count(time_filter) %}
     count(1)
 {% endmacro %}
 
-{% macro re_data_metric_freshness(time_filter, config) %}
+{% macro re_data_metric_freshness(time_filter) %}
     {{ freshness_expression(time_filter) }}
 {% endmacro %}
 
@@ -11,18 +11,18 @@
     {{ adapter.dispatch('freshness_expression', 're_data')(time_column) }}
 {% endmacro %}
 
-{% macro default__freshness_expression(time_column, config) %}
+{% macro default__freshness_expression(time_column) %}
    EXTRACT(EPOCH FROM ({{time_window_end()}} - max({{time_column}})))
 {% endmacro %}
 
-{% macro bigquery__freshness_expression(time_column, config) %}
+{% macro bigquery__freshness_expression(time_column) %}
     TIMESTAMP_DIFF ( timestamp({{ time_window_end()}}), timestamp(max({{time_column}})), SECOND)
 {% endmacro %}
 
-{% macro snowflake__freshness_expression(time_column, config) %}
+{% macro snowflake__freshness_expression(time_column) %}
    timediff(second, max({{time_column}}), {{- time_window_end() -}})
 {% endmacro %}
 
-{% macro redshift__freshness_expression(time_column, config) %}
+{% macro redshift__freshness_expression(time_column) %}
    DATEDIFF(second, max({{time_column}}), {{- time_window_end() -}})
 {% endmacro %}
