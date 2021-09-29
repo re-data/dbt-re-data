3438ebf9db574b05526f33204a2e3ff26b9aaff9
Deji Ibrahim <deji.a.ibrahim@gmail.com>
2021-09-29 22:01:01 +0100

    add equivalent regexp test for bigquery and snowflake

--- integration_tests/macros/my_metrics.sql
+++ integration_tests/macros/my_metrics.sql
@@ -25,14 +25,45 @@
 {% endmacro %}
 
 {% macro re_data_metric_regexp_test(column_name, config) %}
+    {{ regexp_test(column_name, config) }}
+{% endmacro %}
+
+{% macro regexp_test(column_name, config) %}
+    {{ adapter.dispatch('regexp_test', project_name)(column_name, config) }}
+{% endmacro %}
+
+{% macro default__regexp_test(column_name, config) %}
     {% set pattern = config.get('regexp') %}
     coalesce(
         sum(
-            case when event_type ~ '{{pattern}}'
+            case when {{column_name}} ~ '{{pattern}}'
                 then 1
             else 0
             end
         ), 0
     )
+{% endmacro %}
 
+{% macro bigquery__regexp_test(column_name, config) %}
+    {% set pattern = config.get('regexp') %}
+    coalesce(
+        sum(
+            case when regexp_contains({{column_name}}, '{{pattern}}')
+                then 1
+            else 0
+            end
+        ), 0
+    )
+{% endmacro %}
+
+{% macro snowflake__regexp_test(column_name, config) %}
+    {% set pattern = config.get('regexp') %}
+    coalesce(
+        sum(
+            case when regexp_like({{column_name | upper}}, '{{pattern}}')
+                then 1
+            else 0
+            end
+        ), 0
+    )
 {% endmacro %}
