12d409fd36a442f08dffd9659e0763ae5fa5cecf
Deji Ibrahim <deji.a.ibrahim@gmail.com>
2021-10-20 23:50:12 +0100

    fix error causing dbt run to fail

--- macros/utils/mock/empy_tables.sql
+++ macros/utils/mock/empy_tables.sql
@@ -79,3 +79,16 @@
     from dummy_table
     where some_num = 2
 {% endmacro %}
+
+{% macro empty_columns_table() %}
+    {{ re_data.dummy_to_select() }}
+    select 
+        cast (some_string as {{ string_type() }} ) as table_name,
+        cast (some_string as {{ string_type() }} ) as column_name,
+        cast (some_string as {{ string_type() }} ) as data_type,
+        cast (some_bool as {{ boolean_type() }} ) as is_nullable,
+        cast (some_bool as {{ boolean_type() }} ) as is_datetime,
+        cast (some_string as {{ string_type() }} ) as time_filter
+    from dummy_table
+    where some_num = 2
+{% endmacro %}
