88ae4d91166d180e5457a6bdccf96782291eb626
sergey <sergey.a.vdovin@gmail.com>
2021-11-06 01:16:21 +0300

    get_duplicates rc1

--- macros/public/filtering/get_duplicates.sql
+++ macros/public/filtering/get_duplicates.sql
@@ -1,40 +1,31 @@
 {% https://github.com/re-data/re-data/issues/143 %}
 
-{%
+{#
 macro returns rows with she same key set (unique_cols)
 
 along with the fields of the base model duplicates information added:
 re_data_duplicates_count - total number of duplicates with the same current key set
 re_data_duplicate_row_number - number of a duplicate row inside the group of duplicates with the same current key set
-%}
-
-
-
-
-{% to be moved to re_data_utils? %}
-{% macro comma_delimited_list(args) %}
-    {%- for arg in args %} 
-        {{- arg -}} {{- ", " if not loop.last else "" -}}
-    {% endfor %}
-{% endmacro %}}
-
+#}
 
 {% macro filter_get_duplicates(relation, unique_cols, sort_columns) %}
     (
         with with_row_num_count as (
-            select *, row_number() over (
-                partition by {{ re_data.comma_delimited_list(unique_cols) }} {% if sort_columns %} order by {{ re_data.comma_delimited_list(sort_columns) }} {% endif %}
-            ) as re_data_duplicate_row_number
+            select *
             , count(*) over (
+            partition by {{ re_data.comma_delimited_list(unique_cols) }} 
+            ) as re_data_duplicates_group_rows_count
+            , row_number() over (
                 partition by {{ re_data.comma_delimited_list(unique_cols) }} {% if sort_columns %} order by {{ re_data.comma_delimited_list(sort_columns) }} {% endif %}
-            ) as re_data_duplicates_count
+            ) as re_data_duplicates_group_row_number
+
            
             from {{ relation }}
         ),
         duplicate_rows as (
-            select * from with_row_num_count where re_data_duplicates_count > 1
+            select * from with_row_num_count where re_data_duplicates_group_rows_count > 1
         )
-        {% return surrogate key as well? %}
+        {# return surrogate key as well? #}
         select *
         from duplicate_rows
     ) 
