8ba09c59548bd32eb77b374ae1247d47ea3344d9
sergey <sergey.a.vdovin@gmail.com>
2021-11-15 22:06:41 +0300

    https://github.com/re-data/re-data/pull/150#discussion_r749194422
    singular for row_count, duplicate_count and, probably, for most of the prs in the future

--- macros/public/filtering/get_duplicates.sql
+++ macros/public/filtering/get_duplicates.sql
@@ -4,7 +4,7 @@
 macro returns rows with she same key set (unique_cols)
 
 along with the fields of the base model duplicates information added:
-re_data_duplicates_count - total number of duplicates with the same current key set
+re_data_duplicate_count - total number of duplicates with the same current key set
 re_data_duplicate_row_number - number of a duplicate row inside the group of duplicates with the same current key set
 #}
 
@@ -14,7 +14,7 @@ re_data_duplicate_row_number - number of a duplicate row inside the group of dup
             {{re_data.add_duplication_context(relation, unique_cols, sort_columns)}}
         ),
         duplicate_rows as (
-            select * from duplication_context where re_data_duplicates_group_rows_count > 1
+            select * from duplication_context where re_data_duplicate_group_row_count > 1
         )
         {# return surrogate key as well? #}
         select *
--- macros/utils/deduplication/add_duplication_context.sql
+++ macros/utils/deduplication/add_duplication_context.sql
@@ -3,10 +3,10 @@
             select {{ dbt_utils.star(from=relation) }}
             , count(*) over (
                  partition by {{ re_data.comma_delimited_list(unique_cols) }} 
-            ) as re_data_duplicates_group_rows_count
+            ) as re_data_duplicate_group_row_count
             , row_number() over (
                 partition by {{ re_data.comma_delimited_list(unique_cols) }} {% if sort_columns %} order by {{ re_data.comma_delimited_list(sort_columns) }} {% endif %}
-            ) as re_data_duplicates_group_row_number
+            ) as re_data_duplicate_group_row_number
 
             from {{ relation }}
 
