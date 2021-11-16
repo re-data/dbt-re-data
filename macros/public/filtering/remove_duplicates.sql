ffa54f313fbb319f8bc5bfa7f8403f7d87e6b3bf
sergey <sergey.a.vdovin@gmail.com>
2021-11-16 22:37:28 +0300

    test fix

--- macros/public/filtering/remove_duplicates.sql
+++ macros/public/filtering/remove_duplicates.sql
@@ -7,7 +7,7 @@
             {{re_data.add_duplication_context(relation, unique_cols, sort_columns)}}
         ),
         one_row_num as (
-            select * from with_row_num where re_data_duplicates_group_row_number = 1
+            select * from with_row_num where re_data_duplicate_group_row_number = 1
         )
         select {{ dbt_utils.star(from=relation) }}
         from one_row_num
