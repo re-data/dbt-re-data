4822f766f8bf8d5140121f2f94fb4c2cda1e1e2d
Deji Ibrahim <deji.a.ibrahim@gmail.com>
2021-12-06 02:38:27 +0100

    add other overview columns

--- models/internal/re_data_overview.sql
+++ models/internal/re_data_overview.sql
@@ -1,16 +1,31 @@
-
+{{
+    config(
+        materialized='incremental',
+        unique_key = 'generated_at'
+    )
+}}
 
 -- depends_on: {{ ref('re_data_alerting') }}
 -- depends_on: {{ ref('re_data_base_metrics') }}
 -- depends_on: {{ ref('re_data_schema_changes') }}
 
 {% if execute %}
-    {{ log((row_to_json(ref('re_data_base_metrics'))), info=True) }}
+    {% set dbt_graph = tojson(graph) %}
+
     with json_metrics
         as ( select {{ (row_to_json(ref('re_data_base_metrics'))) }} as json_row
-           from {{ ref('re_data_base_metrics') }} )
+           from {{ ref('re_data_base_metrics') }} ),
+        json_anomalies as ( select {{ (row_to_json(ref('re_data_alerting'))) }} as json_row 
+            from {{ ref('re_data_alerting') }} ),
+        json_schema_changes as ( select {{ (row_to_json(ref('re_data_schema_changes'))) }} as json_row 
+            from {{ ref('re_data_schema_changes') }} )
     
-    select {{ agg_to_single_aray('json_row') }} as json_overview from json_metrics
+    select 
+        (select {{ agg_to_single_aray('json_row') }} from json_anomalies) as anomalies,
+        (select {{ agg_to_single_aray('json_row') }} from json_metrics) as metrics,
+        (select {{ agg_to_single_aray('json_row') }} from json_schema_changes) as schema_changes,
+        {{ quote_text(dbt_graph)}} graph,
+        {{ dbt_utils.current_timestamp_in_utc() }} as generated_at
     
     
 {% else %}
