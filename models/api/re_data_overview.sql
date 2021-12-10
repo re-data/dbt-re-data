3c47accb76e934106fa4b90257d496f94807fea4
Mateusz Klimek <matkk11@gmail.com>
2021-12-10 16:44:05 +0100

    Fixes

--- models/api/re_data_overview.sql
+++ models/api/re_data_overview.sql
@@ -1,6 +1,6 @@
 {{
     config(
-        materialized='incremental',
+        materialized='table',
         unique_key = 'generated_at'
     )
 }}
@@ -13,23 +13,29 @@
 {% if execute %}
     {% set dbt_graph = tojson(graph) %}
     
-    with json_metrics
-    as ( select {{ (row_to_json(ref('re_data_base_metrics'))) }} as json_row
-        from {{ ref('re_data_base_metrics') }} ),
-    json_anomalies as ( select {{ (row_to_json(ref('re_data_alerting'))) }} as json_row 
-        from {{ ref('re_data_alerting') }} ),
-    json_schema_changes as ( select {{ (row_to_json(ref('re_data_schema_changes'))) }} as json_row 
-        from {{ ref('re_data_schema_changes') }} ),
-    json_schema as ( select {{ row_to_json(ref('re_data_columns')) }} as json_row 
-        from {{ ref('re_data_columns') }} )
-    
-    select 
-        (select {{ agg_to_single_aray('json_row') }} from json_anomalies) as anomalies,
-        (select {{ agg_to_single_aray('json_row') }} from json_metrics) as metrics,
-        (select {{ agg_to_single_aray('json_row') }} from json_schema_changes) as schema_changes,
-        (select {{ agg_to_single_aray('json_row') }} from json_schema) as table_schema,
-        {{ quote_text(dbt_graph)}} graph,
-        {{ dbt_utils.current_timestamp_in_utc() }} as generated_at
+        with json_metrics
+        as ( select {{ (row_to_json(ref('re_data_base_metrics'))) }} as json_row
+           from {{ ref('re_data_base_metrics') }} ),
+        json_anomalies as ( select {{ (row_to_json(ref('re_data_alerting'))) }} as json_row 
+            from {{ ref('re_data_alerting') }} ),
+        casted_schem_changes as (
+            select id, table_name, operation, column_name, data_type, {{ bool_to_string('is_nullable') }}, prev_column_name, prev_data_type, {{ bool_to_string('prev_is_nullable') }}, detected_time
+            from {{ ref('re_data_schema_changes') }}
+        ),
+        json_schema_changes as ( select {{ (row_to_json(ref('re_data_schema_changes'))) }} as json_row 
+            from casted_schem_changes),
+        casted_columns as (
+            select id, table_name, column_name, data_type, {{ bool_to_string('is_nullable') }}, {{ bool_to_string('is_datetime') }}, time_filter from {{ ref('re_data_columns') }} 
+        ),
+        json_schema as ( select {{ row_to_json(ref('re_data_columns')) }} as json_row 
+            from casted_columns
+        )
+
+        select 'metric', json_row from json_metrics union all
+        select 'anomaly', json_row from json_anomalies union all
+        select 'schema_change', json_row from json_schema_changes union all
+        select 'schema', json_row from json_schema
+        select 'graph', dbt_graph
     
 {% else %}
     {{ re_data.empty_overview_table() }}
