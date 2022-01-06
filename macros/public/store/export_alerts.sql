3c9d6e4eeea27ac18c00dd8c3114c6ed09e3f5e6
Deji Ibrahim <deji.a.ibrahim@gmail.com>
2022-01-06 11:51:30 +0100

    add logic for generating messages for alerts

--- macros/public/store/export_alerts.sql
+++ macros/public/store/export_alerts.sql
@@ -1,13 +1,13 @@
--- depends_on: {{ ref('re_data_alerting') }}
-
 {% macro export_alerts(start_date, end_date) %}
     {% set alerts_query %}
         select
-            {{ overview_select_base('alert', 'computed_on')}}
-            {{ to_single_json(['id', 'metric', 'z_score_value', 'last_value', 'last_avg', 'last_stddev', 'time_window_end', 'interval_length_sec']) }} as {{ re_data.quote_column('data') }}
-        from
-            {{ ref('re_data_alerting') }}
-            where date(time_window_end) between '{{start_date}}' and '{{end_date}}'
+            *
+        from {{ ref('re_data_alerts') }}
+        where
+            case
+                when type = 'anomaly' then time_window_end between '{{ start_date }}' and '{{ end_date }}'
+                else true
+            end
     {% endset %}
 
     {% set query_result = run_query(alerts_query) %}
