6b777cd2496aa21a31812d410eddd6c409b1b8a7
Deji Ibrahim <deji.a.ibrahim@gmail.com>
2022-01-21 17:41:36 +0100

    use ddl commands to drop schema for redshift

--- integration_tests/macros/drop_all_schemas.sql
+++ integration_tests/macros/drop_all_schemas.sql
@@ -7,8 +7,25 @@
         schema_name + '_expected',
         schema_name + '_dbt_test__audit'
     ] %}
+    {{ adapter.dispatch('drop_all_schemas')(schemas_to_drop) }}
+{% endmacro %}
+
+{% macro default__drop_all_schemas(schemas_to_drop) %}
     {% for schema in schemas_to_drop %}
         {% set relation = api.Relation.create(database=target.database, schema=schema) %}
         {% do adapter.drop_schema(relation) %}
     {% endfor %}
+{% endmacro %}
+
+{% macro redshift__drop_all_schemas(schemas_to_drop) %}
+    {# 
+        dropping schemas with adapter.drop_schema doesn't seem to work with redshift
+        so we default to issuing DDL commands to redshift
+    #}
+    {% set drop_query %}
+        {% for schema in schemas_to_drop %}
+            drop schema if exists {{schema}} cascade;
+        {% endfor %}
+    {% endset %}
+    {% do run_query(drop_query) %}
 {% endmacro %}
