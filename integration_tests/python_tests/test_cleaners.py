b8f5d70f6b0eac1c50498a1410d00b0a674f68a0
Sergey Vdovin <sergey.a.vdovin@gmail.com>
2022-02-10 23:22:04 +0300

    seeds to one command

--- integration_tests/python_tests/test_cleaners.py
+++ integration_tests/python_tests/test_cleaners.py
@@ -6,8 +6,7 @@ def test_cleaners(db, debug=True):
 
     print (f"Running setup and tests for {db}")
 
-    dbt_seed('--select sample_user_data expected_sample_user_data', db)
-    dbt_seed('--select customers_to_impute expected_customers_imputed', db)
+    dbt_seed('--select sample_user_data expected_sample_user_data customers_to_impute expected_customers_imputed', db)
     
     dbt_run('--select sanitized_user_data+', db)
     dbt_test('--select sanitized_user_data', db)
