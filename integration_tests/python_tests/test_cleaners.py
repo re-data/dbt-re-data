7a734d92e2d4a6c9099edb63adc6ac7a7a60d733
sergey <sergey.a.vdovin@gmail.com>
2022-02-10 22:22:54 +0300

    test fix

--- integration_tests/python_tests/test_cleaners.py
+++ integration_tests/python_tests/test_cleaners.py
@@ -10,7 +10,7 @@ def test_cleaners(db, debug=True):
     dbt_run('--select sanitized_user_data+', db)
     dbt_test('--select sanitized_user_data', db)
 
-    dbt_seed('--select expected_customers_imputed', db)
+    dbt_seed('--select customers_to_impute expected_customers_imputed', db)
     dbt_run('--select imputed_customers_data+', db)
     dbt_test('--select imputed_customers_data', db)
     lf 
