9c2e28c06ef7d25f45c6f63f31859469be250918
Mateusz Klimek <mateusz@getre.io>
2022-04-12 20:54:18 +0200

    fix typo

--- macros/utils/mock/empty_tables.sql
+++ macros/utils/mock/empty_tables.sql
@@ -6,7 +6,7 @@
             cast (null as {{ long_string_type() }}) as long_string_type,
             cast (1 as {{ numeric_type() }}) as numeric_type,
             cast ('2000-01-10' as {{ timestamp_type() }}) as timestamp_type,
-            cast (true as {{ boolean_type() }}) as boolean_typem
+            cast (true as {{ boolean_type() }}) as boolean_type
     )
 {% endmacro %}
 
