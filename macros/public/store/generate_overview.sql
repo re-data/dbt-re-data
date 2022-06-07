cc3afd925ac40c8b24acffe1dbce237153e18ea3
Mateusz Klimek <mateusz@getre.io>
2022-06-07 16:11:13 +0200

    Update

--- macros/public/store/generate_overview.sql
+++ macros/public/store/generate_overview.sql
@@ -88,6 +88,6 @@
     {% set overview_result = run_query(overview_query) %}
     {% set overview_file_path = overview_path or 'target/re_data/overview.json' %}
     {% do overview_result.to_json(overview_path) %}
-    {% do save_monitored(monitored_path) %}
+    {{ save_monitored(monitored_path) }}
 
 {% endmacro %}
