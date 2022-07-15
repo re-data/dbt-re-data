86cfaf4a9eb233f42800b7878d3abe5e20caa17b
Mateusz Klimek <mateusz@getre.io>
2022-07-15 09:36:33 +0200

    Fix for recursion problem in dbt compile/dbt docs generate

--- macros/utils/json_extract.sql
+++ macros/utils/json_extract.sql
@@ -1,11 +1,11 @@
 {% macro json_extract(string, string_path) -%}
 
-{{ adapter.dispatch('json_extract', 're_data') (string, string_path) }}
+{{ adapter.dispatch('json_extract_impl', 're_data') (string, string_path) }}
 
 {%- endmacro %}
 
 -- use fivetran_utils by default
-{% macro default__json_extract(string, string_path) %}
+{% macro default__json_extract_impl(string, string_path) %}
 
   {{ fivetran_utils.json_extract(string, string_path) }}
 
--- macros/utils/percentile.sql
+++ macros/utils/percentile.sql
@@ -1,11 +1,11 @@
 {% macro percentile(percentile_field, partition_field, percent) -%}
 
-{{ adapter.dispatch('percentile', 're_data') (percentile_field, partition_field, percent) }}
+{{ adapter.dispatch('percentile_impl', 're_data') (percentile_field, partition_field, percent) }}
 
 {%- endmacro %}
 
 -- use fivetran_utils by default
-{% macro default__percentile(percentile_field, partition_field, percent)  %}
+{% macro default__percentile_impl(percentile_field, partition_field, percent)  %}
     {{ fivetran_utils.percentile(percentile_field, partition_field, percent) }}
 {% endmacro %}
 
