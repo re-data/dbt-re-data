79a59b2828c2ee85520061e2f731537cf126f8e5
Mateusz Klimek <mateusz@getre.io>
2022-07-20 15:47:53 +0200

    make in_date_window work like before

--- macros/public/store/export_tests_history.sql
+++ macros/public/store/export_tests_history.sql
@@ -15,7 +15,7 @@
             compiled_sql
         from
             {{ ref('re_data_test_history') }}
-        where {{ in_date_window('run_at', start_date, end_date) }} 
+        where {{ in_date_window('run_at', start_date, end_date) }}
     {% endset %}
 
     {% set query_result = run_query(tests_history_query) %}
