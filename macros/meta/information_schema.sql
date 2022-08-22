f9fd2be19eb4acff7e0c5e898cd298a5a2814536
Mateusz Klimek <mateusz@getre.io>
2022-08-22 10:37:53 +0200

    Improve implemention for redshift (#90)

--- macros/meta/information_schema.sql
+++ macros/meta/information_schema.sql
@@ -3,6 +3,7 @@
 {% endmacro %}
 
 {% macro default__get_monitored_columns(table_schema, db_name) %}
+    {% set relation = api.Relation.create(database=db_name, schema=table_schema) %}
     select
         table_name,
         table_schema,
@@ -11,94 +12,21 @@
         data_type,
         is_nullable
     from
-    {{ tables_in_schema(table_schema, db_name) }}
+        {{ relation.information_schema('COLUMNS') }}
+    where
+        table_schema = '{{ table_schema }}'
 {% endmacro %}
 
 {% macro redshift__get_monitored_columns(table_schema, db_name) %}
-
-    {%- call statement('columns', fetch_result=True) -%}
-    with bound_views as (
-        select
-            table_name,
-            table_schema,
-            table_catalog,
-            column_name,
-            data_type,
-            is_nullable
-        from
-            {% if db_name %}{{db_name}}.{% endif %} information_schema.columns
-    ),
-    external_views as (
-        select
-            table_name,
-            table_schema,
-            table_catalog,
-            column_name,
-            data_type,
-            is_nullable
-        from
-            pg_catalog.svv_columns
-    ),
-    unioned as (
-        select * from bound_views
-        union all
-        select * from external_views
-    )
-
     select
-        column_name,
-        data_type,
         table_name,
         table_schema,
         table_catalog,
+        column_name,
+        data_type,
         is_nullable
     from
-        unioned
+        svv_columns
     where
         table_schema = '{{ table_schema }}'
-    {% endcall %}
-
-    {%- set columns = load_result('columns')['data'] -%}
-    {% set temp_table_name = 're_data_temporary_monitored_columns_for_' + table_schema %}
-
-    {% set create_temp_table_query %}
-        create temp table {{ temp_table_name }} (
-            table_name {{ string_type()}},
-            table_schema {{ string_type()}},
-            table_catalog {{ string_type()}},
-            column_name {{ string_type()}},
-            data_type {{ string_type() }},
-            is_nullable {{ string_type() }}
-        );
-        insert into {{ temp_table_name }}  values
-        {% for col in columns %} (
-            '{{col[0]}}'::text,
-            '{{col[1]}}'::text,
-            '{{col[2]}}'::text,
-            '{{col[3]}}'::text,
-            '{{col[4]}}'::text,
-            '{{col[5]}}'::text
-            ) {%- if not loop.last %}, {%- endif %}
-        {% endfor %}
-
-    {% endset %}
-    {% do run_query(create_temp_table_query) %}
-
-    select table_name, table_schema, table_catalog, column_name, data_type, is_nullable
-    from {{ temp_table_name }} 
-
-{% endmacro %}
-
-{% macro tables_in_schema(schema_name, db_name) %}
-    {{ adapter.dispatch('tables_in_schema', 're_data')(schema_name, db_name) }}
-{% endmacro %}
-
-{% macro default__tables_in_schema(schema_name, db_name) %}
-    {% if db_name %} {{ db_name}} . {% endif %} INFORMATION_SCHEMA.columns
-    where
-    table_schema = '{{ schema_name }}'
-{% endmacro %}
-
-{% macro bigquery__tables_in_schema(schema_name, db_name) %}
-    {% if db_name %} `{{ db_name}}` . {% endif %} `{{schema_name}}` . `INFORMATION_SCHEMA`.`COLUMNS`
 {% endmacro %}
