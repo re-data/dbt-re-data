c2bf3b86cf5d7e6a083e47c51949651b3a0d0e10
NGUYEN HUU BACH <bachng@gmail.com>
2022-10-29 10:14:08 +0000

    add namespace to fivetran_utils macros, fix #103

--- macros/utils/fivetran_utils/percentile.sql
+++ macros/utils/fivetran_utils/percentile.sql
@@ -5,7 +5,7 @@
 
 {% macro percentile(percentile_field, partition_field, percent) -%}
 
-{{ adapter.dispatch('percentile') (percentile_field, partition_field, percent) }}
+{{ adapter.dispatch('percentile','re_data') (percentile_field, partition_field, percent) }}
 
 {%- endmacro %}
 
