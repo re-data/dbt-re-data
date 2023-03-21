9804ec3b92ed8a899c3694ea081e97e7a98d9b36
NGUYEN HUU BACH <bachng@gmail.com>
2023-03-21 13:12:29 +0000

    add filter to anomaly detection https://github.com/re-data/re-data/issues/392

--- models/alerts/re_data_anomalies.sql
+++ models/alerts/re_data_anomalies.sql
@@ -3,6 +3,16 @@
         materialized='view'
     )
 }}
+
+{% set sql %}
+select
+name,schema,database,{{json_extract('anomaly_detector','filter')}} filter
+from {{ ref('re_data_selected') }}
+where json_extract_scalar(anomaly_detector,'$.filter') is not null
+{% endset %}
+
+{%- set filters = dbt_utils.get_query_results_as_dict(sql) -%}
+
 select
     z.id,
     z.table_name,
@@ -28,21 +38,29 @@ select
     {{ re_data.generate_anomaly_message('z.column_name', 'z.metric', 'z.last_value', 'z.last_avg') }} as message,
     {{ re_data.generate_metric_value_text('z.metric', 'z.last_value') }} as last_value_text
 from
-    {{ ref('re_data_z_score')}} z 
-left join {{ ref('re_data_selected') }} m 
+    {{ ref('re_data_z_score')}} z
+left join {{ ref('re_data_selected') }} m
 on {{ split_and_return_nth_value('table_name', '.', 1) }} = m.database
 and {{ split_and_return_nth_value('table_name', '.', 2) }} = m.schema
 and {{ split_and_return_nth_value('table_name', '.', 3) }} = m.name
 where
-    case 
-        when {{ json_extract('m.anomaly_detector', 'name') }} = 'z_score' 
+    case
+    {% for filter in filters["filter"] %}
+      when m.database='{{ filters["database"][loop.index0] }}' and m.schema='{{ filters["schema"][loop.index0] }}' and m.name='{{ filters["name"][loop.index0] }}'
+      then {{ filter }}
+    {% endfor %}
+    else true
+    end
+    and
+    case
+        when {{ json_extract('m.anomaly_detector', 'name') }} = 'z_score'
             then abs(z_score_value) > cast({{ json_extract('m.anomaly_detector', 'threshold') }} as {{ numeric_type() }})
-        when {{ json_extract('m.anomaly_detector', 'name') }} = 'modified_z_score' 
+        when {{ json_extract('m.anomaly_detector', 'name') }} = 'modified_z_score'
             then abs(modified_z_score_value) > cast( {{ json_extract('m.anomaly_detector', 'threshold') }} as {{numeric_type()}} )
-        when {{ json_extract('m.anomaly_detector', 'name') }} = 'boxplot' 
+        when {{ json_extract('m.anomaly_detector', 'name') }} = 'boxplot'
             then (
                 z.last_value < z.last_first_quartile - (cast( {{ json_extract('m.anomaly_detector', 'whisker_boundary_multiplier') }} as {{numeric_type()}} ) * z.last_iqr)
-                or 
+                or
                 z.last_value > z.last_third_quartile + (cast( {{ json_extract('m.anomaly_detector', 'whisker_boundary_multiplier') }} as {{numeric_type()}} ) * z.last_iqr)
             )
         else false
